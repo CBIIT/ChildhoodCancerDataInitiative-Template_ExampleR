dfd417a7d638a145326b231236428b7656911b4d
svburke <sean.burke2@nih.gov>
2023-09-13 14:55:17 -0500

    Update CCDI-Template_ExampleR.R

--- CCDI-Template_ExampleR.R
+++ CCDI-Template_ExampleR.R
@@ -281,6 +281,10 @@ for (node in sheet_names){
   }
   df=df[-1,]
   rownames(df)<-NULL
+  
+  #get rid of the 'id' column values, they are not required.
+  df["id"]=NA
+  
   #add data frame back into workbook list
   workbook_list[node]=list(df)
 }
@@ -324,19 +328,22 @@ for (node in sheet_names){
     df_cols=colnames(df)
     for (col_name in df_cols){
       if (grepl(pattern = "\\.", x = col_name)){
-        prev_node=unlist(stri_split_fixed(str = col_name, pattern = "."))[1]
-        prev_prop=unlist(stri_split_fixed(str = col_name, pattern = "."))[2]
-        df_prev=workbook_list[prev_node][[1]]
-        value_prev=df_prev[prev_prop][[1]]
-        if(length(value_prev)<dim(df[col_name])[1]){
-          fit_size=dim(df[col_name])[1]
-          actual_size=length(value_prev)
-          multi=trunc(fit_size/actual_size)+1
-          value_prev=rep(x = value_prev, multi)
-          value_prev=value_prev[1:fit_size]
+        #remove the [node].id columns from this as well
+        if( !grepl(pattern = '\\.id', x = col_name)){
+          prev_node=unlist(stri_split_fixed(str = col_name, pattern = "."))[1]
+          prev_prop=unlist(stri_split_fixed(str = col_name, pattern = "."))[2]
+          df_prev=workbook_list[prev_node][[1]]
+          value_prev=df_prev[prev_prop][[1]]
+          if(length(value_prev)<dim(df[col_name])[1]){
+            fit_size=dim(df[col_name])[1]
+            actual_size=length(value_prev)
+            multi=trunc(fit_size/actual_size)+1
+            value_prev=rep(x = value_prev, multi)
+            value_prev=value_prev[1:fit_size]
+          }
+          df[col_name]<-value_prev
+          workbook_list[node][[1]]=df
         }
-        df[col_name]<-value_prev
-        workbook_list[node][[1]]=df
       }
     }
   }
@@ -350,8 +357,10 @@ for (node in sheet_names){
     df=workbook_list[node][[1]]
     df_cols=colnames(df)
     if(length(grep(pattern = "\\.", x = df_cols))>1){
-      conn_len=length(grep(pattern = "\\.", x = df_cols))
       conn_val=df_cols[grep(pattern = "\\.", x = df_cols)]
+      #remove the [node].id columns, we don't want to include those
+      conn_val=conn_val[!grepl(pattern = "\\.id", x = conn_val)]
+      conn_len=length(conn_val)
       for (x in 1:num_of_random){
         col_ran=round(runif(n = 1, min = 1, max = conn_len))
         conn_val_rans=conn_val[-col_ran]
